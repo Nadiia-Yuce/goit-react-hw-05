7d271fc28c8942fc3ff3df3c78fa1306434f4ed1
Nadiia Yuce <soyka9616@gmail.com>
2024-09-09 09:30:03 +0200

    add not found page layout

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import css from "./NotFoundPage.module.css";
 
 export default function NotFoundPage() {
   return (
     <div>
-      <h1>Page is not found!</h1>
-      <Link to="/">Go back</Link>
+      <h1 className={css.title}>Error 404! Page is not found!</h1>
+      <div className={css.back}>
+        <Link to="/">Go back</Link>
+      </div>
     </div>
   );
 }
